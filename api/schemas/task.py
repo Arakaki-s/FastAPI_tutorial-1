410d93c07f0aca3eba9159920c6ea01be6b62b0f
Arakaki-s <shuichi.arakaki@gmail.com>
2022-07-24 02:20:02 +0900

    task.py修正

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -15,7 +15,7 @@ class TaskCreateResponse(TaskCreate):
 
 class Task(TaskBase):
     id: int
-    done: bool = Field(False, description="完了フラグ")
+    done: bool = Field(False, description="完了")
 
     class Config:
         orm_mode = True
